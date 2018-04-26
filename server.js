66e0606242e8eb9aa6426a3cfef453486e20556c
Khoa Huynh <khoawin92@gmail.com>
2018-04-25 17:32:43 -0700

    testing api get

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const superagent = require('superagent');
+const pg = require('pg');
+const fs = require('fs');
+const cors = require('cors');
+const express = require('express');
+const PORT = process.env.PORT;
+const app = express();
+const API_KEY = process.env.API_KEY;
+// const conString = 'postgres://postgres:3874@LOCALHOST:5432/DBNAME';
+// const client = new pg.Client(conString);
+// client.connect();
+// client.on('error', err => {
+// console.error(err);
+// });
+
+app.use(cors({origin: '*'}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+// app.use(express.static('./public'));
+
+
+app.get('/api/yelp/v3/sushi/98121', (req, res) => {
+  superagent.get('https://api.yelp.com/v3/businesses/search?term=sushi&location=98121&limit=50')
+    .set('Authorization', `Bearer ${API_KEY}`)
+    // .then(results => {
+    //   var offset = Math.floor(Math.random()*JSON.parse(results.text).total);
+    // } )
+    .then(results => res.send(JSON.parse(results.text)))
+    .catch(err => console.log(err));
+});
+
+
+
+ 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
