9f058e1eacfb6715bf6eadc96805480f55a0345d
Khoa Huynh <khoawin92@gmail.com>
2018-04-26 18:31:19 -0700

    fixed get requests to be more specific

--- server.js
+++ server.js
@@ -14,31 +14,41 @@ const API_KEY = process.env.API_KEY;
 // console.error(err);
 // });
 
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('./public'));
 
 
-app.get('/api/yelp/v3/sushi/98121', (req, res) => {
-  superagent.get('https://api.yelp.com/v3/businesses/search?term=sushi&location=98121&limit=50&price=$$$$')
+// app.get('/api/yelp/v3/sushi/98121', (req, res) => {
+//   superagent.get('https://api.yelp.com/v3/businesses/search?categories=sushi&location=98121&limit=50&price=$$$$')
+//     .set('Authorization', `Bearer ${API_KEY}`)
+//     // .then(results => {
+//     //   var offset = Math.floor(Math.random()*JSON.parse(results.text).total);
+//     // } )
+//     .then(results => res.send(JSON.parse(results.text)))
+//     .catch(err => console.log(err));
+// });
+
+
+
+//Changed open now to false to test categories after hours 
+app.get('/api/yelp/v3/:food/:zip/:price/:range/:offset', (req, res) => {
+  superagent.get(`https://api.yelp.com/v3/businesses/search?open_now=true&categories=${req.params.food}&location=${req.params.zip}&limit=50&price=${req.params.price}&radius=${req.params.range}&offset=${req.params.offset}`)
     .set('Authorization', `Bearer ${API_KEY}`)
-    // .then(results => {
-    //   var offset = Math.floor(Math.random()*JSON.parse(results.text).total);
-    // } )
     .then(results => res.send(JSON.parse(results.text)))
     .catch(err => console.log(err));
 });
-
-app.get('/api/yelp/v3/:food/:zip/:price/:range', (req, res) => {
-  superagent.get(`https://api.yelp.com/v3/businesses/search?open_now=true&term=${req.params.food}&location=${req.params.zip}&limit=50&price=${req.params.price}&radius=${req.params.range}`)
-  .set('Authorization', `Bearer ${API_KEY}`)
-  .then(results => res.send(JSON.parse(results.text)))
-  .catch(err => console.log(err));
+//Changed open now to false to test categories after hours
+app.get('/total/api/yelp/v3/:food/:zip/:price/:range', (req, res) => {
+  superagent.get(`https://api.yelp.com/v3/businesses/search?open_now=true&categories=${req.params.food}&location=${req.params.zip}&limit=50&price=${req.params.price}&radius=${req.params.range}`)
+    .set('Authorization', `Bearer ${API_KEY}`)
+    .then(results => {
+      res.send(JSON.stringify(JSON.parse(results.text).total));
+    })
+    .catch(err => console.log(err));
 });
 
 
 
-
- 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
