a69997a7d410ea4166b3aaba62295ea167346167
Khoa Huynh <khoawin92@gmail.com>
2018-10-03 17:30:47 -0700

    added return for superagent

--- src/routes/yelpRouter.js
+++ src/routes/yelpRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 //Changed open now to false to test categories after hours 
 router.get('/api/v3/yelp/:food/:location/:price/:range/:offset', (req, res) => {
-  superagent.get(`https://api.yelp.com/v3/businesses/search?categories=${req.params.food}&${req.params.location}&limit=50&price=${req.params.price}&radius=${req.params.range}&offset=${req.params.offset}`)
+  return superagent.get(`https://api.yelp.com/v3/businesses/search?categories=${req.params.food}&${req.params.location}&limit=50&price=${req.params.price}&radius=${req.params.range}&offset=${req.params.offset}`)
     .set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
     .then(results => res.send(JSON.parse(results.text)))
     .catch(err => console.error(err));
